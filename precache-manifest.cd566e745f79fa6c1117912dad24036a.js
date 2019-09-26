308050e28b808bd704e9128f9d7057686026fc90
Ryan White <mrwhiterk@gmail.com>
2019-09-26 10:36:51 -0400

    Updates

---
+++ precache-manifest.cd566e745f79fa6c1117912dad24036a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db33444b8af547f7c812045b816caa1f",
+    "url": "/github-watchlist-react-app/index.html"
+  },
+  {
+    "revision": "3600d93dcbaa580ca676",
+    "url": "/github-watchlist-react-app/static/css/main.21c610e4.chunk.css"
+  },
+  {
+    "revision": "1bc3ba670109595a1649",
+    "url": "/github-watchlist-react-app/static/js/2.13b9c2a0.chunk.js"
+  },
+  {
+    "revision": "3600d93dcbaa580ca676",
+    "url": "/github-watchlist-react-app/static/js/main.55ec7c96.chunk.js"
+  },
+  {
+    "revision": "104660d81e9636f87dde",
+    "url": "/github-watchlist-react-app/static/js/runtime-main.d6648d09.js"
+  }
+]);
